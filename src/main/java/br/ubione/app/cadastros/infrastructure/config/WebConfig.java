f274efd9f8ae31c940fd7a786f02b9a00a53c0e6
Ubiratan Passos <ubiratan.passos@neoviaengenharia.com.br>
2025-08-14 15:58:34 -0300

    ajuste - CORS

--- src/main/java/br/ubione/app/cadastros/infrastructure/config/WebConfig.java
+++ src/main/java/br/ubione/app/cadastros/infrastructure/config/WebConfig.java
@@ -7,18 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .exposedHeaders("Authorization")
-                        .allowCredentials(true);
+                    .allowedOriginPatterns("*") // <- aqui permite qualquer origem (substitui allowedOrigins)
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
             }
         };
     }
